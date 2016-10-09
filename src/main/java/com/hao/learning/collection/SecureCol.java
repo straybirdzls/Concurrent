2657a7b58b1975ec12299362f3489ec93f832e3c
lingxue <lingxue@mogujie.com>
2016-10-09 16:46:12 +0800

    add sucure coll

---
+++ src/main/java/com/hao/learning/collection/SecureCol.java
@@ -0,0 +1,17 @@
+package com.hao.learning.collection;
+
+import java.util.Collections;
+import java.util.Vector;
+
+/**
+ * @author lingxue created on 10/9/16
+ * @version v0.1
+ **/
+
+public class SecureCol {
+
+    public static void main(String[] args){
+
+
+    }
+}
